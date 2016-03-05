1ebd84786df1493415c93418c1e3ad5243ba90ba
huoxudong125 <423huo@163.com>
2016-03-05 21:00:01 +0800

    init Solution

---
+++ src/HQF.Tools.QuartzAdmin/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HQF.Tools.QuartzAdmin.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
